63bfc65a8b0fcab3a753341b3aefd787a26ce052
Maxime Suret <msuret@dhatim.com>
2016-01-19 19:26:42 +0100

    add license information

--- src/test/java/org/dhatim/dropwizard/jwt/cookie/authentication/TestResource.java
+++ src/test/java/org/dhatim/dropwizard/jwt/cookie/authentication/TestResource.java
@@ -1,4 +1,19 @@
-package com.example.trying;
+/**
+ * Copyright 2016 Dhatim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.dhatim.dropwizard.jwt.cookie.authentication;
 
 import io.dropwizard.auth.Auth;
 import javax.ws.rs.Consumes;
@@ -9,9 +24,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import org.dhatim.dropwizard.jwt.cookie.authentication.DontRefreshSession;
-import org.dhatim.dropwizard.jwt.cookie.authentication.JwtCookieSecurityContext;
-import org.dhatim.dropwizard.jwt.cookie.authentication.Subject;
 
 @Path("subject")
 public class TestResource {
