3d0acfbca0b426af9d9e58c8a1713594342fe5c4
Maxime Suret <msuret@dhatim.com>
2016-08-24 19:10:31 +0200

    remove unused @JsonProperty annotation

--- src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/DefaultJwtCookiePrincipal.java
+++ src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/DefaultJwtCookiePrincipal.java
@@ -26,112 +26,126 @@ import java.util.Optional;
 /**
  * Default implementation of JwtCookiePrincipal
  */
-public class DefaultJwtCookiePrincipal implements JwtCookiePrincipal{
-    
+public class DefaultJwtCookiePrincipal implements JwtCookiePrincipal {
+
     private final static String PERSISTENT = "pst"; // long-term token == rememberme
     private final static String ROLES = "rls";
-    
+
     protected final Claims claims;
 
     /**
      * Builds a new instance of DefaultJwtCookiePrincipal
+     *
      * @param name the principal name
      * @param persistent if the cookie must be persistent
      * @param roles the roles the principal is in
      * @param claims custom data associated with the principal
      */
-    @JsonCreator
-    public DefaultJwtCookiePrincipal(@JsonProperty("name")String name, @JsonProperty("persistent")boolean persistent, @JsonProperty("roles")Collection<String> roles, @JsonProperty("claims")Claims claims){
+    public DefaultJwtCookiePrincipal(
+            @JsonProperty("name") String name,
+            @JsonProperty("persistent") boolean persistent,
+            @JsonProperty("roles") Collection<String> roles,
+            @JsonProperty("claims") Claims claims) {
         this.claims = Optional.ofNullable(claims).orElseGet(Jwts::claims);
         this.claims.setSubject(name);
         this.claims.put(PERSISTENT, persistent);
         this.claims.put(ROLES, roles);
     }
-    
+
     /**
      * Build a new instance of DefaultJwtCookiePrincipal with the given name
+     *
      * @param name the name
      */
-    public DefaultJwtCookiePrincipal(String name){
+    public DefaultJwtCookiePrincipal(String name) {
         this(name, false, Collections.emptyList(), null);
     }
-    
+
     /**
      * Build a new instance of DefaultJwtCookiePrincipal from the JWT claims
+     *
      * @param claims the JWT claims
      */
-    public DefaultJwtCookiePrincipal(@JsonProperty("claims") Claims claims) {
+    public DefaultJwtCookiePrincipal(Claims claims) {
         this.claims = claims;
     }
 
     /**
      * Get the claims used to serialize this principal
+     *
      * @return the claims
      */
     public Claims getClaims() {
         return claims;
     }
-    
+
     /**
-    * Indicates if this principal has the given role
-    * @param role the role
-    * @return true if the principal is in the given role, false otherwise
-    */
+     * Indicates if this principal has the given role
+     *
+     * @param role the role
+     * @return true if the principal is in the given role, false otherwise
+     */
     @Override
-    public boolean isInRole(String role){
+    public boolean isInRole(String role) {
         return getRoles().contains(role);
     }
-    
+
     /**
      * Get a collection of all the roles this principal is in
+     *
      * @return the roles
      */
-    public Collection<String> getRoles(){
+    public Collection<String> getRoles() {
         return Optional.ofNullable(claims.get(ROLES))
                 .map(Collection.class::cast)
                 .orElse(Collections.emptyList());
     }
-    
+
     /**
      * Set the roles this principal is in
+     *
      * @param roles the roles
      */
-    public void setRoles(Collection<String> roles){
+    public void setRoles(Collection<String> roles) {
         claims.put(ROLES, roles);
     }
-    
+
     /**
      * Indicates if the cookie must be persistent
+     *
      * @return if the cookie must be persistent
      */
     @Override
     public boolean isPersistent() {
         return claims.get(PERSISTENT) == Boolean.TRUE;
     }
-    
+
     /**
      * Set if the cookie must be persistent
+     *
      * @param persistent if the cookie must be persistent
      */
-    public void setPresistent(boolean persistent){
+    public void setPresistent(boolean persistent) {
         claims.put(PERSISTENT, persistent);
     }
 
     /**
      * Get the name of the principal
+     *
      * @return the name
      */
     @Override
     public String getName() {
         return (String) claims.getSubject();
     }
-    
+
     /**
      * Set the name of the principal
+     *
      * @param name the name
      */
     public void setName(String name) {
         claims.setSubject(name);
     }
-    
+
 }
