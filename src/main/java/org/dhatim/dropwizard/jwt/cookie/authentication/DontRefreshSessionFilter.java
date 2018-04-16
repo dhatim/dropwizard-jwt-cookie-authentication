69edd4ccff5f05430b4b5c6dfa163ff5acfc085a
Maxime Suret <msuret@dhatim.com>
2018-04-16 10:22:03 +0200

    expose request & response filter for manual setup

--- src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/DontRefreshSessionFilter.java
+++ src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/DontRefreshSessionFilter.java
@@ -20,13 +20,13 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 
 @DontRefreshSession
-class DontRefreshSessionFilter implements ContainerRequestFilter{
+public class DontRefreshSessionFilter implements ContainerRequestFilter{
 
     public static String DONT_REFRESH_SESSION_PROPERTY = "dontRefreshSession";
-    
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         requestContext.setProperty(DONT_REFRESH_SESSION_PROPERTY, Boolean.TRUE);
     }
-    
+
 }
