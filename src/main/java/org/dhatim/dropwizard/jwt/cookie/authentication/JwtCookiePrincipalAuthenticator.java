9390cfeb46684db6dcf874256322aca9d0aa57fb
Olivier Chédru <ochedru@cegid.com>
2023-03-09 16:08:01 +0100

    Update copyright

--- src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/JwtCookiePrincipalAuthenticator.java
+++ src/main/java/org/dhatim/dropwizard/jwt/cookie/authentication/JwtCookiePrincipalAuthenticator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Dhatim
+ * Copyright 2023 Dhatim
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
